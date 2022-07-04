08df401116091e6149c0a54ab08224a541ccddfb
Nanda Eka Suci Ramadan <97726832+nandaeka02@users.noreply.github.com>
2022-07-04 21:41:01 +0700

    Add wedding web for test

---
+++ weddingweb/script.js
@@ -0,0 +1,34 @@
+var countDownDate = new Date("Jul 2, 2022 19:30:00").getTime();
+const day = document.getElementById("day");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  minute.innerHTML = minutes;
+  second.innerHTML = seconds;
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
